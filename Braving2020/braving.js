61e66490535b137dbc10ba3d807a1c79256e06fe
jbrenae <greene.jalisa@yahoo.com>
2024-11-10 05:05:30 -0500

    Add files via upload

---
+++ Braving2020/braving.js
@@ -0,0 +1,8 @@
+function toggleNavBar() {
+  var n = document.getElementById("navbar");
+  if (n.style.display === "block") {
+    n.style.display = "none";
+  } else {
+    n.style.display = "block";
+  }
+}
